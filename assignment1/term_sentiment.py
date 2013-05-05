065ce6bb49885303ad1fcfc5b2b01c2c738c71f2
angeloce <garchek@gmail.com>
2013-05-05 18:20:16 +0800

    problem 2: tweet sentiment

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,31 @@
+#coding:utf-8
+
 import sys
+
 
 def hw():
     print 'Hello, world!'
 
+
 def lines(fp):
     print str(len(fp.readlines()))
+
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
+
+    for line in sent_file:
+        sd = line.split()
+        if len(sd) > 2:
+            print line
+
     hw()
     lines(sent_file)
     lines(tweet_file)
 
+
 if __name__ == '__main__':
+    sys.argv.append('AFINN-111.txt')
+    sys.argv.append('head_20_output.txt')
     main()
--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,66 @@
+#coding:utf-8
+
 import sys
+import codecs
+import json
 
-def hw():
-    print 'Hello, world!'
 
-def lines(fp):
-    print str(len(fp.readlines()))
+phrase_sentiment_sheet = {}
+word_sentiment_sheet = {}
+
+
+def set_sentiment_sheet(filename):
+    sent_file = codecs.open(sys.argv[1], encoding="utf-8")
+
+    for line in sent_file:
+        word, score = line.split('\t', 1)
+        try:
+            word = word.strip()
+            score = float(score.strip())
+        except:
+            continue
+        if ' ' in word:
+            first_word = word.split(' ', 1)[0]
+            if first_word not in phrase_sentiment_sheet:
+                phrase_sentiment_sheet[first_word] = {}
+            phrase_sentiment_sheet[first_word][word] = score
+        else:
+            word_sentiment_sheet[word] = score
+
+    sent_file.close()
+
+
+def print_sentiment(filename):
+    tweet_file = codecs.open(filename, encoding="utf-8")
+    for line in tweet_file:
+        sentiment = 0
+        tweet = json.loads(line).get('text')
+        if tweet:
+            index = 0
+            while index < len(tweet):
+                word_end_index = tweet.find(' ', index)
+                if word_end_index < 0:
+                    break
+                word = tweet[index:word_end_index]
+
+                if word in word_sentiment_sheet:
+                    sentiment += word_sentiment_sheet[word]
+                elif word in phrase_sentiment_sheet:
+                    for phrase in phrase_sentiment_sheet[word]:
+                        if tweet[index:].startswith(phrase):
+                            word_end_index = tweet.find(' ', index + len(phrase))
+                            sentiment += phrase_sentiment_sheet[word][phrase]
+                            break
+                index = word_end_index + 1
+        print sentiment
+
+    tweet_file.close()
+
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    set_sentiment_sheet(sys.argv[1])
+    print_sentiment(sys.argv[2])
+
 
 if __name__ == '__main__':
     main()
