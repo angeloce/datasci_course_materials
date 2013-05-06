eeee50d6611804761c50a78091d7fc78a5165a7e
angeloce <garchek@gmail.com>
2013-05-06 16:27:03 +0800

    problem 5: find happiest state in us

---
+++ assignment1/happiest_state.py
@@ -0,0 +1,97 @@
+#coding:utf-8
+
+import sys
+import codecs
+import json
+
+
+phrase_sentiment_sheet = {}
+word_sentiment_sheet = {}
+
+
+def is_wanted_tweet(tweet):
+    if tweet:
+        try:
+            if tweet.get('text') and tweet.get('place') and tweet.get('lang') == 'en':
+                if tweet['place']['country_code'] == 'US' and tweet['place']['full_name']:
+                    return True
+        except:
+            return False
+    return False
+
+
+def set_sentiment_sheet(filename):
+    sent_file = codecs.open(sys.argv[1], encoding="utf-8")
+
+    for line in sent_file:
+        word, score = line.split('\t', 1)
+        try:
+            word = word.lower().strip()
+            score = float(score.strip())
+        except:
+            continue
+        if ' ' in word:
+            first_word = word.split(' ', 1)[0]
+            if first_word not in phrase_sentiment_sheet:
+                phrase_sentiment_sheet[first_word] = {}
+            phrase_sentiment_sheet[first_word][word] = score
+        else:
+            word_sentiment_sheet[word] = score
+
+    sent_file.close()
+
+
+def get_sentiment(tweet):
+    sentiment = 0
+    index = 0
+    if tweet:
+        tweet = tweet.lower()
+        while index < len(tweet):
+            word_end_index = tweet.find(' ', index)
+            if word_end_index < 0:
+                word_end_index = len(tweet)
+            word = tweet[index:word_end_index]
+
+            if word in word_sentiment_sheet:
+                sentiment += word_sentiment_sheet[word]
+            # phrase match
+            elif word in phrase_sentiment_sheet:
+                for phrase in phrase_sentiment_sheet[word]:
+                    if tweet[index:].startswith(phrase):
+                        word_end_index = tweet.find(' ', index + len(phrase))
+                        sentiment += phrase_sentiment_sheet[word][phrase]
+                        break
+            index = word_end_index + 1
+    return sentiment
+
+
+def print_happiest_state(filename):
+    tweet_file = codecs.open(filename, encoding="utf-8")
+
+    state_sentiment = {}
+    for line in tweet_file:
+        tweet = json.loads(line).get('text')
+        if is_wanted_tweet(tweet):
+            state = tweet['place']['full_name'][-2:]
+            sentiment = get_sentiment(tweet)
+            if state not in state_sentiment:
+                state_sentiment[state] = 0
+            state_sentiment[state] += sentiment
+
+    tweet_file.close()
+
+    # happiest_state
+    if state_sentiment:
+        print max(state_sentiment.items(), key=lambda x: x[1])[0]
+    else:
+        print "US"
+
+
+def main():
+    set_sentiment_sheet(sys.argv[1])
+    print_happiest_state(sys.argv[2])
+
+
+if __name__ == '__main__':
+    main()
+
--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -10,7 +10,6 @@ word_sentiment_sheet = {}
 
 unset_term_scores = {}
 
-
 def set_sentiment_sheet(filename):
     sent_file = codecs.open(sys.argv[1], encoding="utf-8")
 
@@ -41,30 +40,15 @@ def _is_term(term):
 
 
 def find_unset_term_score(tweet):
-    global unset_term_scores
     sentiment = 0
-    index = 0
     unset_terms = set()
     if tweet:
-        tweet = ' '.join(tweet.strip().lower().split())
-        while index < len(tweet):
-            word_end_index = tweet.find(' ', index)
-            if word_end_index < 0:
-                word_end_index = len(tweet)
-            word = tweet[index:word_end_index]
-
+        words = tweet.strip().split()
+        for word in words:
             if word in word_sentiment_sheet:
                 sentiment += word_sentiment_sheet[word]
-            # phrase match
-            elif word in phrase_sentiment_sheet:
-                for phrase in phrase_sentiment_sheet[word]:
-                    if tweet[index:].startswith(phrase):
-                        word_end_index = tweet.find(' ', index + len(phrase))
-                        sentiment += phrase_sentiment_sheet[word][phrase]
-                        break
-            elif _is_term(word):  # word is not in AFINN-111.txt
+            elif _is_term(word):
                 unset_terms.add(word)
-            index = word_end_index + 1
 
     for term in unset_terms:
         if term not in unset_term_scores:
